d080f07ff2224b62f642cdeaa880b48a7de9776c
Gabriel <gabriel@pop-os.localdomain>
2022-09-01 20:06:24 -0300

    Criando campo de busca

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from '../../components/SearchForm/'
 
 export function Home() {
   return (
     <div>
       <Header />
       <Summary />
+      <SearchForm />
     </div>
   )
 }
---
+++ src/components/SearchForm/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  display: flex;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    background-color: ${({ theme }) => theme['gray-900']};
+    border: 0;
+    padding: 1rem;
+    border-radius: 6px;
+    color: ${({ theme }) => theme['gray-300']};
+    font-size: 16px;
+
+    &::placeholder {
+      color: ${({ theme }) => theme['gray-500']};
+    }
+  }
+
+  button {
+    padding: 0.875rem 2rem;
+    background: transparent;
+    color: ${({ theme }) => theme['green-300']};
+    border: 1px solid ${({ theme }) => theme['green-300']};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    border-radius: 6px;
+
+    &:hover {
+      color: ${({ theme }) => theme.white};
+      background-color: ${({ theme }) => theme['green-300']};
+      transition: background-color 0.2s, color 0.2s;
+    }
+  }
+`
